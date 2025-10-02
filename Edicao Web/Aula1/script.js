260f56ef1777b91fb60491e64cb989fd08549d7b
Carolina Pinto <carolinaop.co2003@gmail.com>
2025-10-02 12:24:32 +0100

    feat: add initial HTML and JavaScript files for Aula1

---
+++ Edicao Web/Aula1/script.js
@@ -0,0 +1,47 @@
+let myLuckNumber = 7;
+const myName = "Carolina";
+
+console.log("Nome: " + myName + "\nNúmero da sorte: " + myLuckNumber);
+
+while (myLuckNumber < 10) {
+  myLuckNumber++;
+  console.log("Número da sorte atualizado: " + myLuckNumber);
+}
+
+console.log(" ");
+
+let year = "1998";
+year = year + 1;
+console.log(year);
+
+const city = "Kiyoto";
+const country = "Japan";
+const combo = city + country;
+console.log(combo);
+
+const animal = "hippopotamus";
+console.log(animal[7]);
+
+let curso = "Front End";
+console.log(curso[5]);
+
+console.log(" ");
+
+const message = "            TASTE THE RAINBOW!     ";
+console.log(message);
+whisper = message.trim().toLowerCase();
+console.log(whisper);
+
+console.log(" ");
+
+const word = "skateboard";
+console.log(word);
+let subword = word.substring(5, 10);
+console.log(subword);
+let facialHair = subword.replace("o", "e");
+console.log(facialHair);
+
+console.log(" ");
+
+let promptedName = prompt("Qual é o seu nome?");
+console.log(promptedName);
